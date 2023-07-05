e9cb1066b7fefbd676bb271e56a81abee1023344
Vink77 <vink.av@mail.ru>
2023-07-05 18:05:53 +0700

    add init

--- homework-4/main.py
+++ homework-4/main.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     print(video1.__dict__)
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
     print("\nvideo2")
-    print(video2)
+    print(video2.channel)
+    print(video2.__dict__)
+
 
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
