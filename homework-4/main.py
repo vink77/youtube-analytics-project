0162113499da6a8082f04caae43a6c03d8eb0e86
Vink77 <vink.av@mail.ru>
2023-07-06 10:31:57 +0700

    end

--- homework-4/main.py
+++ homework-4/main.py
@@ -3,13 +3,6 @@ from src.video import Video, PLVideo
 if __name__ == '__main__':
     # Создаем два экземпляра класса
     video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
-    print(video1)
-    print(video1.__dict__)
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    print("\nvideo2")
-    print(video2.url)
-    print(video2.__dict__)
-
-
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
